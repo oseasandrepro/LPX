3b14a0cbb8701973e6583e998fea0b118ba4667c
Oseas <oseasandrepro@gmail.com>
2023-04-25 14:16:49 -0400

    Experimentos de Ite e Ger em C#

---
+++ Experimentos/Iteradores_VS_Geradores_C#/Gerador/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        foreach (int prime in GetPrimes())
+        {
+            Console.WriteLine(prime);
+            Thread.Sleep(1000);
+        }
+    }
+
+    public static IEnumerable<int> GetPrimes()
+    {
+        int currPrime = 1;
+        while (true)
+        {
+            currPrime++;
+            if (IsPrimeNumber(currPrime))
+            {
+                yield return currPrime;
+            }
+        }
+    }
+
+    private static bool IsPrimeNumber(int value)
+    {
+        if (value != 2 && value % 2 == 0)
+        {
+            return false;
+        }
+
+        for (int i = 2; i < value / 2; i++)
+        {
+            if (value % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Experimentos/Iteradores_VS_Geradores_C#/Iteradores/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrimeIterator primeNumbers = new PrimeIterator();
+
+       foreach (int prime in primeNumbers)
+       {
+           Console.WriteLine(prime);
+            Thread.Sleep(1000);
+       }
+    }
+
+}
+
+public class PrimeIterator : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        return new PrimeEnumerator();
+    }
+
+}
+
+public class PrimeEnumerator : IEnumerator
+{
+    private int currPrime = 1;
+    public object Current
+    {
+        get { return currPrime; }
+    }
+
+    public bool MoveNext()
+    {
+        while (true)
+        {
+            currPrime++;
+            if (isPrimeNumber(currPrime)){
+               break;
+            }
+        }
+
+        return true;
+    }
+
+    public void Reset()
+    {
+        throw new NotSupportedException();
+    }
+
+    private static bool isPrimeNumber(int value)
+    {
+        if (value != 2 && value % 2 == 0)
+            return false;
+
+        for (int i = 2; i < value / 2; i++)
+        {
+            if (value % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
