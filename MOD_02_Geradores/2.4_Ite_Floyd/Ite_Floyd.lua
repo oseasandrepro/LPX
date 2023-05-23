d9971a1d4d551e48f75f456f2e028e4897b9b9e3
Oseas <oseasandrepro@gmail.com>
2023-05-23 07:23:35 -0300

    Floyd foi um fiasco, organizando

---
+++ MOD_02_Geradores/2.4_Ite_Floyd/Ite_Floyd.lua
@@ -0,0 +1,22 @@
+--[[
+	2.4 - Criar um iterador para o Floyd
+	
+	"Read lines of text, until a completely blank line is found.
+	Eliminate redundant blanks between the words.
+	Print the text, thirty characters to a line, without breaking words between lines."
+	
+	-Entrada em um bloco de linhas (parágrafo) por vez
+	-Saída em um linha de 30 colunas por vez
+	-As linhas não devem ter espaços redundantes
+--]]
+
+function Ite_Floyd(filename)
+
+	
+end
+
+print(string.rep('.',30))
+for l in Ite_Floyd("lorem.txt") do
+    print(l)
+end
+print()
